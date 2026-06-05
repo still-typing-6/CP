fd9875bb52832bd37822c82553b9b66debfb2ad1
still-typing-6 <anshukumar01687@gmail.com>
2026-06-05 16:05:03 +0530

    Create K - Divisors.cpp

---
+++ K - Divisors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+  int n;
+  cin>>n;
+  for(int i = 1;i<=n;i++){
+    if(n%i == 0){
+      cout<<i<<endl;
+    }
+  }
+  return 0;
+}
