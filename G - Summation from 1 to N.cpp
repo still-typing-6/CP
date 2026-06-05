0eeb2edf43a8b5ae14fac835af1197e0533617a6
still-typing-6 <anshukumar01687@gmail.com>
2026-06-05 15:39:22 +0530

    Add program to calculate summation from 1 to N

---
+++ G - Summation from 1 to N.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+  long long int N;
+  cin>>N;
+  long long int sum = ((N)*(N+1))/2;
+  cout<<sum<<endl;
+ return 0;
+}
